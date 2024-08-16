105519738e6e542094fefd4a2350f0a979292057
Aditya <adityakumarji74@gmail.com>
2024-08-16 21:15:28 +0530

    changes

--- src/components/About/AboutSectionThree.tsx
+++ src/components/About/AboutSectionThree.tsx
@@ -1,4 +1,5 @@
 import SectionTitle from "../Common/SectionTitle";
+import Image from "next/image"; // Import the Image component
 
 const AboutSectionThree = () => {
   return (
@@ -14,16 +15,19 @@ const AboutSectionThree = () => {
         <div className="flex flex-wrap justify-center gap-8">
           <div
             className="mx-auto mb-8 w-full sm:w-[90%] md:w-[45%]"
-            data-aos="fade-left"
+            data-aos="fade-up"
+            data-aos-anchor-placement="bottom-bottom"
           >
             <div className="rounded-lg bg-white p-8 shadow-two duration-300 hover:shadow-one dark:bg-dark dark:shadow-three dark:hover:shadow-gray-dark">
               <h3 className="mb-4 pb-3 text-center text-xl font-semibold text-dark dark:text-white">
                 Mode 1: Steven He's Dad's Meme Madness
               </h3>
-              <img
+              <Image
                 src="/trashtalks-stevenhe.png"
                 alt="Mode 1"
-                className="mx-auto mb-4 h-auto w-[50%] max-w-xs rounded-lg sm:max-w-xs md:max-w-sm lg:max-w-xs"
+                className="mx-auto mb-4 h-auto  w-[50%] max-w-xs rounded-lg sm:max-w-xs md:max-w-sm lg:max-w-xs"
+                width={500} // Adjust width
+                height={300} // Adjust height
               />
               <p className="mb-6 border-b border-body-color border-opacity-10 pb-6 text-center text-base leading-relaxed text-body-color dark:border-white dark:border-opacity-10 dark:text-white sm:text-lg">
                 Dive into meme-driven hilarity with Steven He's Dad! Type
@@ -36,18 +40,21 @@ const AboutSectionThree = () => {
 
           <div
             className="mx-auto mb-8 w-full sm:w-[90%] md:w-[45%]"
-            data-aos="fade-right"
+            data-aos="fade-up"
+            data-aos-anchor-placement="bottom-bottom"
           >
             <div className="rounded-lg bg-white p-8 shadow-two duration-300 hover:shadow-one dark:bg-dark dark:shadow-three dark:hover:shadow-gray-dark">
               <h3 className="mb-4 pb-3 text-center text-xl font-semibold text-dark dark:text-white">
                 Mode 2: Gordon Ramsay Roasting
               </h3>
-              <img
+              <Image
                 src="/trashtalks-gordon.png"
                 alt="Mode 2"
                 className="mx-auto mb-4 h-auto w-[50%] max-w-xs rounded-lg sm:max-w-xs md:max-w-sm lg:max-w-xs"
+                width={500} // Adjust width
+                height={300} // Adjust height
               />
-              <p className="mb-6 border-b border-body-color border-opacity-10 pb-6 text-center text-base leading-relaxed text-body-color dark:border-white dark:border-opacity-10 dark:text-white sm:text-lg">
+              <p className="mb-6 border-b border-body-color border-opacity-10 pb-6  text-center text-base leading-relaxed text-body-color dark:border-white dark:border-opacity-10 dark:text-white sm:text-lg">
                 Type "Chef" to summon Gordon Ramsay and receive comically
                 scathing remarks and hilarious critiques. Get ready for a
                 virtual kitchen roast with the fiery chef’s iconic insults. It's
